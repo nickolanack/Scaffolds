79ea97ce23c1700d1f5cb1f63d78e6e9ceb9477b
nickolanack <nickblackwell82@gmail.com>
2015-08-26 11:46:09 -0700

    add php, some tests

--- phpunit/ScaffoldsTest.php
+++ phpunit/ScaffoldsTest.php
@@ -5,7 +5,19 @@ class ScaffoldsTest extends PHPUnit_Framework_TestCase {
     /**
      * @runInSeparateProcess
      */
+    public function testIncludehelper() {
+        include_once '../scaffolds/scaffolds/defines.php';
+        
+        $this->assertEquals(true, class_exists('Scaffolding'));
+        $this->assertEquals(true, function_exists('HTML'));
+    }
+
+    /**
+     * @runInSeparateProcess
+     */
     public function testInclude() {
-        $this->assertEquals(true, true);
+        include_once '../scaffolds/scaffolds/scaffolding.php';
+        $this->assertEquals(true, class_exists('Scaffolding'));
+        $this->assertEquals(false, function_exists('HTML'));
     }
 }
