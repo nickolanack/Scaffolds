99e71669aa6948700807eda97351ad9bb78dcfc5
nickolanack <nickblackwell82@gmail.com>
2015-08-26 11:59:12 -0700

    oh oops, had triplicated a test...

--- phpunit/ScaffoldsTest.php
+++ phpunit/ScaffoldsTest.php
@@ -24,15 +24,6 @@ class ScaffoldsTest extends PHPUnit_Framework_TestCase {
     /**
      * @runInSeparateProcess
      */
-    public function testInclude() {
-        include_once dirname(__DIR__) . '/scaffolds/scaffolding.php';
-        $this->assertEquals(true, class_exists('Scaffolding'));
-        $this->assertEquals(false, function_exists('HTML'));
-    }
-
-    /**
-     * @runInSeparateProcess
-     */
     public function testScaffoldDocument() {
         include_once dirname(__DIR__) . '/scaffolds/defines.php';
         
