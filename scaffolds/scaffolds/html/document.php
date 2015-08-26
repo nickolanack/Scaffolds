63d9a44ae6ba56d8df162c39b23ccce5a3d5e829
nickolanack <nickblackwell82@gmail.com>
2015-08-26 11:08:36 -0700

    added mobile device-width meta

--- scaffolds/scaffolds/html/document.php
+++ scaffolds/scaffolds/html/document.php
@@ -1,93 +1,94 @@
-<?php 
+<?php
+$config = array_merge(
+    array(
+        'header' => null,
+        'body' => null,
+        'buffered' => true,
+        
+        // the following optional params can be set by the header callback function
+        'title' => false,
+        'description' => false,
+        'keywords' => false,
+        'base' => false,
+        'icon' => false,
+        'generator' => false
+    ), $params);
 
-
-$config=array_merge(array(
-		'header'=>null,
-		'body'=>null,
-		'buffered'=>true, 
-		
-		//the following optional params can be set by the header callback function 
-		'title'=>false,
-		'description'=>false,
-		'keywords'=>false,
-		'base'=>false,
-		'icon'=>false,
-		'generator'=>false
-	),$params);
-
-if(!($config['header']&&$config['header'] instanceof Closure)){
-	throw new Exception('Expected $params[\'header\']=function(){} in '.__FILE__);
+if (! ($config['header'] && $config['header'] instanceof Closure)) {
+    throw new Exception('Expected $params[\'header\']=function(){} in ' . __FILE__);
 }
 
-
-if(!($config['body']&&$config['body'] instanceof Closure)){
-	throw new Exception('Expected $params[\'body\']=function(){} in '.__FILE__);
+if (! ($config['body'] && $config['body'] instanceof Closure)) {
+    throw new Exception('Expected $params[\'body\']=function(){} in ' . __FILE__);
 }
 
-
-$body=function()use($config){
-	
-	$config['body']();
-	
+$body = function () use($config) {
+    
+    $config['body']();
 };
 
-if($config['buffered']){
-	ob_start();
-	$body();
-	$body=ob_get_contents();
-	ob_end_clean();
+if ($config['buffered']) {
+    ob_start();
+    $body();
+    $body = ob_get_contents();
+    ob_end_clean();
 }
 
-
-
-
-?><!DOCTYPE HTML>
+?>
+<!DOCTYPE HTML>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php
-		
-		if($config['base']){
-			?><base href="<?php echo $config['base']; ?>" />
-					<?php
-		}
-		
-			$config['header']();
-		
-		if($config['title']){
-			?><title><?php echo htmlspecialchars($config['title']);?></title>
-			<?php 
-		}
 
-		if($config['description']){
-			?><meta name="description" content="<?php echo htmlspecialchars($config['description']);?>" />
-			<?php 
-		}
-		
-		if($config['keywords']){
-			?><meta name="keywords" content="<?php echo htmlspecialchars($config['keywords']);?>" />
+if ($config['base']) {
+    ?><base href="<?php echo $config['base']; ?>" />
+					<?php
+}
+
+$config['header']();
+
+if ($config['title']) {
+    ?><title><?php echo htmlspecialchars($config['title']);?></title>
 			<?php
-		}
-		
-		if($config['generator']){
-			?><meta name="generator" content="<?php echo htmlspecialchars($config['generator']);?>" />
+}
+
+if ($config['description']) {
+    ?><meta name="description"
+	content="<?php echo htmlspecialchars($config['description']);?>" />
 			<?php
-		}
-		
-		if($config['icon']){
-			?><link rel="icon" type="image/<?php echo end(explode('.', ($config['icon'])));?>" href="<?php echo $config['icon']?>"/>
+}
+
+if ($config['keywords']) {
+    ?><meta name="keywords"
+	content="<?php echo htmlspecialchars($config['keywords']);?>" />
 			<?php
-		}?>
-		
+}
+
+if ($config['generator']) {
+    ?><meta name="generator"
+	content="<?php echo htmlspecialchars($config['generator']);?>" />
+			<?php
+}
+
+if ($config['icon']) {
+    ?><link rel="icon"
+	type="image/<?php echo end(explode('.', ($config['icon'])));?>"
+	href="<?php echo $config['icon']?>" />
+			<?php
+}
+?>
+
 	</head>
-	<body>
+<body>
 		<?php
-			if($config['buffered']){
-				echo $body;
-			}else{
-				$body();
-			}
-		?>
+if ($config['buffered']) {
+    echo $body;
+} else {
+    $body();
+}
+?>
 	</body>
-</html><?php 
+</html><?php
 ?>
