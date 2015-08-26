b7f04ab4e794839f2c19c118dd1b37c1a2c9908e
nickolanack <nickblackwell82@gmail.com>
2015-08-26 11:40:50 -0700

    add travis test files

---
+++ phpunit/ScaffoldsTest.php
@@ -0,0 +1,11 @@
+<?php
+
+class ScaffoldsTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testInclude() {
+        $this->assertEquals(true, true);
+    }
+}
